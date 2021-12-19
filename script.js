e56c346c05768a5cd9bf977e7d809310c75b9edc
Orestis Psycharis <opsychar@gmail.com>
2021-12-19 20:33:14 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+var body = document.querySelectorAll("body");
+let starCount = 0;
+
+const createStar = () => {
+    let star = document.createElement("div");
+    star.className = "star";
+    star.style = `top:${Math.floor(
+        Math.random() * 100 - 1
+    )}%; left:${Math.floor(Math.random() * 100 - 1)}%`;
+    body[0].firstElementChild.appendChild(star.cloneNode(true));
+};
+
+const createSky = () => {
+    if (starCount < 250) {
+        setTimeout(() => {
+            createStar();
+            starCount++;
+            createSky();
+        }, 30);
+    }
+};
+
+createSky();
+
+const fallingStar = () => {
+    if (body[0].firstElementChild.childNodes[0].className !== "star") {
+        fallingStar();
+    }
+    setTimeout(() => {
+        body[0].firstElementChild.childNodes[0].id = "star";
+
+        fallingStar();
+    }, 5000);
+
+    if (body[0].firstElementChild.childNodes[0].id == "star") {
+        setTimeout(() => {
+            body[0].firstElementChild.childNodes[0].remove();
+            createStar();
+        }, 4000);
+    }
+};
+
+setTimeout(() => {
+    fallingStar();
+}, 1000);
